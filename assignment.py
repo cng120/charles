6136147900e7b6b46950a9947d78b7f84bf2560d
charles ngugi <charlesngugi@gmail.com>
2025-11-06 11:04:05 +0300

    nemisis protocol

---
+++ assignment.py
@@ -0,0 +1,7 @@
+#program that converts number of hours into seconds
+print("1.Convert hours into seconds")
+
+if choice  ==1:
+    number of hours=float(input("Enter the number of hours"))
+    seconds=float(number of hours*3600)
+    print ("convert hours into seconds",seconds)
